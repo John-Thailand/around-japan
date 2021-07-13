0cc35cf8dd203e2cbac6fa43118d7feb2fee340b
Apple <apple@appurunoMacBook-puro.local>
2021-07-13 19:08:24 +0900

    create Map Page

--- lib/menu/menu_page.dart
+++ lib/menu/menu_page.dart
@@ -1,116 +1,53 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'menu_model.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MenuPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final mailController = TextEditingController();
-    final passwordController = TextEditingController();
-
-    return ChangeNotifierProvider<MenuModel>(
-      create: (_) => MenuModel(),
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue.withOpacity(0.7),
-          title: Text('メニュー',
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-              )),
-        ),
-        body: Consumer<MenuModel>(
-          builder: (context, model, child) {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    margin: const EdgeInsets.only(top: 35),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'メールアドレス',
-                        hintText: 'example@ex.com',
-                        prefixIcon: Icon(Icons.mail_outline),
-                      ),
-                      controller: mailController,
-                      onChanged: (text) {
-                        model.mail = text;
-                      },
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(top: 35),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'パスワード',
-                        hintText: 'P@ssw0rd',
-                        prefixIcon: Icon(Icons.password),
-                      ),
-                      obscureText: true,
-                      controller: passwordController,
-                      onChanged: (text) {
-                        model.password = text;
-                      },
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(top: 45),
-                    width: 190,
-                    child: RaisedButton(
-                      padding: const EdgeInsets.all(5),
-                      child: Text(
-                        'ログインする',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 24,
-                        ),
-                      ),
-                      onPressed: () async {
-                        try {
-                          await model.Menu();
-                          _showDialog(context, 'ログインしました');
-                        } catch (e) {
-                          _showDialog(context, e.toString());
-                        }
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            );
-          },
-        ),
-      ),
-    );
+    return MapSample();
   }
 }
 
-Future _showDialog(BuildContext context, String title) async {
-  showDialog<void>(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: Text(title),
-        actions: <Widget>[
-          TextButton(
-            child: const Text('OK'),
-            onPressed: () {
-              Navigator.of(context).pop();
-              if (title == 'ログインしました') {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => MenuPage(),
-                  ),
-                );
-              }
-            },
-          ),
-        ],
-      );
-    },
+class MapSample extends StatefulWidget {
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(35.68134947200318, 139.76724281456794),
+    zoom: 14.4746,
   );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
+      ),
+    );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
 }
