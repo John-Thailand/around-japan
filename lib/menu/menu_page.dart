883b8efbae46c76aed789e4dece732db6a7dc361
Apple <apple@appurunoMacBook-puro.local>
2021-07-19 21:03:37 +0900

    create goal button

--- lib/menu/menu_page.dart
+++ lib/menu/menu_page.dart
@@ -109,6 +109,92 @@ class MenuPageState extends State<MenuPage> {
     });
   }
 
+  bool isGoal = false;
+  // ゴールを押した時の処理
+  void _onAddGoalMarkerButtonPressed() {
+    setState(() {
+      if (markerNum == 0) {
+        // 警告・スタート地点が設定されていません。スタートボタンを押して、スタート地点を設定してください。
+        showDialog<void>(
+          context: context,
+          // 背景を押した時に、ダイアログは閉じない設定
+          barrierDismissible: false,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('警告'),
+              content: Text('スタート地点が設定されていません。\nスタートボタンを押してください。'),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('OK'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      } else {
+        _showSimpleDialog(context);
+        // showDialog 確認・markerNum日目の終了地点として設定しますか？ゴール地点として設定しますか？
+        //                 1. markerNum日目の終了地点として設定
+        //                    title: marker日目の終了地点 content: 最後まで諦めずに突き進みましょう！
+        //                    上記の情報を持ったマーカーを追加する
+        //                    データベースに位置情報を更新する
+        //              if isGoal == false {
+        //                 2. ゴール地点として設定
+        //                    title: ゴール content: 最後までやりきったあなたは素晴らしいです！
+        //                    上記の情報を持ったマーカーを追加する
+        //                    データベースに位置情報を更新する
+        //                    isGoalをtrueに変更する
+        //              } else {
+        //                    警告・ゴールは既に設定されています。 [OK]
+        //                    スタートボタンでデータを消去した場合、isGoalを初期化（false）
+        //                 3. 設定しない
+        //                    Navigator.pop
+      }
+    });
+  }
+
+  Future<void> _showSimpleDialog(BuildContext context) async {
+    String day = markerNum as String;
+    await showDialog<void>(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return SimpleDialog(
+            title: Text('$day日目の終了地点として設定しますか？\nゴール地点として設定しますか？'),
+            children: <Widget>[
+              SimpleDialogOption(
+                onPressed: () {
+                  // title: marker日目の終了地点 content: 最後まで諦めずに突き進みましょう！
+                  // 上記の情報を持ったマーカーを追加する
+                  _addMarker('$day日目の終了地点', '最後まで諦めずに突き進みましょう！');
+                  // データベースに位置情報を更新する
+                  _updateUserPositionToFirestore();
+                  Navigator.pop(context);
+                },
+                child: Text('$day日目の終了地点として設定'),
+              ),
+              SimpleDialogOption(
+                onPressed: () {
+                  // title: ゴール content: 最後までやりきったあなたは素晴らしいです！
+                  // 上記の情報を持ったマーカーを追加する
+                  Navigator.pop(context);
+                },
+                child: Text('ゴール地点として設定'),
+              ),
+              SimpleDialogOption(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('設定しない'),
+              ),
+            ],
+          );
+        });
+  }
+
   void _showDialog(BuildContext context, String title, String content,
       Function function, Function dbfunction) {
     showDialog<void>(
@@ -191,6 +277,27 @@ class MenuPageState extends State<MenuPage> {
     });
   }
 
+  // FireStoreにユーザ情報の位置情報を更新する
+  Future _updateUserPositionToFirestore() async {
+    String email = _getUserEmail();
+    final collection = FirebaseFirestore.instance.collection('userPosition');
+    final snapshot = await collection.get();
+    final docs = snapshot.docs;
+    // それぞれのドキュメント
+    docs.forEach((doc) {
+      // アカウント設定した時のメールアドレスとドキュメント内のメールアドレスが一致している場合
+      if (doc['email'] == email) {
+        // そのドキュメントを更新する
+        collection.doc(doc.id).update({
+          'geopoints': FieldValue.arrayUnion([
+            GeoPoint(_yourLocation!.latitude as double,
+                _yourLocation!.longitude as double)
+          ]),
+        });
+      }
+    });
+  }
+
   // FireStoreにユーザ位置情報を削除する
   Future _deleteUserPositionToFirestore() async {
     String email = _getUserEmail();
@@ -205,7 +312,6 @@ class MenuPageState extends State<MenuPage> {
         collection.doc(doc.id).delete();
       }
     });
-    // collection.where('email', isEqualTo: email).get();
   }
 
   // ユーザーのメールアドレスを取得する
@@ -254,8 +360,11 @@ class MenuPageState extends State<MenuPage> {
                   children: <Widget>[
                     button(_onMapTypeButtonPressed, Icons.map, '地図切替'),
                     SizedBox(height: 16.0),
-                    button(_onAddStartMarkerButtonPressed, Icons.add_location,
-                        'スタート'),
+                    button(_onAddStartMarkerButtonPressed,
+                        Icons.location_on_outlined, 'スタート'),
+                    SizedBox(height: 16.0),
+                    button(_onAddGoalMarkerButtonPressed, Icons.add_location,
+                        'ゴール　'),
                   ],
                 ),
               ),
